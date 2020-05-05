038924cf9bbfdf096a6c39b7c93a5e54f9f27bb4
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-05 19:55:10 +0100

    Setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App/App";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+import App from "./components/App/App";
+import store from "./configureStore";
+
+import "./index.css";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
