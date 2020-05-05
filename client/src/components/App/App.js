1adf541459a67f5451204e2f7dec1c3c4d3f29b5
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-05 19:45:42 +0100

    Add PostList component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import PostList from "../PostList/PostList";
 
 const App = () => {
   return (
     <div>
-      <div>App</div>
+      <PostList />
     </div>
   );
 };
