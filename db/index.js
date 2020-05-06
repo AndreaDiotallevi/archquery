0b662859437600dca22508534baf84183ad20fe8
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-06 18:01:02 +0100

    Deploy to heroku

--- db/index.js
+++ db/index.js
@@ -1,12 +1,21 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
-const pool = new Pool({
-  user: "andrea",
-  host: "localhost",
-  database: "archquery",
-  password: "password",
-  port: 5432,
-});
+const devConfig = {
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+};
+
+const proConfig = {
+  connectionString: process.env.DATABASE_URL, // Coming from Heroku addons
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 module.exports = {
   query: (text, params, callback) => {
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const app = express();
 const path = require("path");
 const mountRoutes = require("./routes");
 
-const app = express();
+// Middleware
 app.use(express.json());
 
+// Routes
 mountRoutes(app);
+
+// Production Setup
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
 module.exports = app;
