d8a13682709f34b7a21f3a211e76c18b1acbcfeb
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-08 11:08:02 +0100

    Add usersReducers to rootReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
-import questions from "./questionsReducers";
+import questionsReducers from "./questionsReducers";
+import usersReducers from "./usersReducers";
 
 export default combineReducers({
-  questions,
+  questions: questionsReducers,
+  users: usersReducers,
   form: formReducer,
 });
