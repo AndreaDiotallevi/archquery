f6b98d51bb9145a2654987b725327e9c9d0d1297
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-08 11:44:04 +0100

    Amend QuestionExcerpt component

--- client/src/components/QuestionExcerpt/QuestionExcerpt.js
+++ client/src/components/QuestionExcerpt/QuestionExcerpt.js
@@ -3,7 +3,7 @@ import React from "react";
 const QuestionExcerpt = ({ body }) => {
   return (
     <div className="component-question-excerpt">
-      <p>{body.substring(0, 200)} ...</p>
+      <p>{`${body.substring(0, 200)} ${body.length > 200 ? "..." : ""}`}</p>
     </div>
   );
 };
