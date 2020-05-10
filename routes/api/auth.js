ffe4af0b911a3ad2b43f3df8e46ffa028a791a9b
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-10 13:31:47 +0100

    Re-organise folders

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("../../auth");
+const passport = require("../../services/passport");
 const db = require("../../db");
 
 module.exports = router;
