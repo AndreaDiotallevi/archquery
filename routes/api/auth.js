5ac6ae844a93a0915f908928d6962f7462300162
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-10 13:08:13 +0100

    Add auth signup route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,40 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const db = require("../../db");
-
-// module.exports = router;
-
-// router.post("/signup", async (req, res) => {
-//   try {
-//     console.log(req.body);
-//     const { username, email, password } = req.body;
-//     const {
-//       rows,
-//     } = await db.query(
-//       "INSERT INTO users (username, email, password) VALUES ($1, $2, crypt($3, gen_salt('bf', 10))) RETURNING *",
-//       [username, email, password]
-//     );
-//     res.status(200).json(rows[0]);
-//   } catch (err) {
-//     res.status(400).json({ msg: err.message });
-//   }
-// });
-
-// router.post("/login", async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
-//     const {
-//       rows,
-//     } = await db.query(
-//       "SELECT * FROM users WHERE username = $1 AND password = crypt($2, password)",
-//       [username, password]
-//     );
-//     res.status(200).json(rows[0]);
-//   } catch (err) {
-//     res.status(400).json({ msg: err.message });
-//   }
-// });
-
 const express = require("express");
 const router = express.Router();
 const db = require("../../db");
@@ -43,33 +6,6 @@ module.exports = router;
 
 var passport = require("passport"),
   LocalStrategy = require("passport-local").Strategy;
-
-// passport.use(
-//   new LocalStrategy(function (username, password, done) {
-//     User.findOne({ username: username }, function (err, user) {
-//       if (err) {
-//         return done(err);
-//       }
-//       if (!user) {
-//         return done(null, false, { message: "Incorrect username." });
-//       }
-//       if (!user.validPassword(password)) {
-//         return done(null, false, { message: "Incorrect password." });
-//       }
-//       return done(null, user);
-//     });
-//   })
-// );
-
-// passport.serializeUser(function (user, done) {
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser(function (id, done) {
-//   User.findById(id, function (err, user) {
-//     done(err, user);
-//   });
-// });
 
 passport.serializeUser(function (user, done) {
   done(null, user.id);
@@ -84,20 +20,7 @@ passport.deserializeUser(async (id, done) => {
   } catch (err) {
     done(err, null);
   }
-  // User.findById(id, function (err, user) {
-  //   done(err, user);
-  // });
 });
-
-// passport.serializeUser((user, done) => {
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser((id, done) => {
-//   knex('users').where({id}).first()
-//   .then((user) => { done(null, user); })
-//   .catch((err) => { done(err,null); });
-// });
 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
@@ -118,34 +41,23 @@ passport.use(
     } catch (err) {
       return done(err);
     }
-    // User.findOne({ username: username }, function (err, user) {
-    //   if (err) {
-    //     return done(err);
-    //   }
-    //   if (!user) {
-    //     return done(null, false, { message: "Incorrect username." });
-    //   }
-    //   if (!user.validPassword(password)) {
-    //     return done(null, false, { message: "Incorrect password." });
-    //   }
-    //   return done(null, user);
-    // });
   })
 );
 
-// passport.use(new LocalStrategy(options, (username, password, done) => {
-//   // check to see if the username exists
-//   knex('users').where({ username }).first()
-//   .then((user) => {
-//     if (!user) return done(null, false);
-//     if (!authHelpers.comparePass(password, user.password)) {
-//       return done(null, false);
-//     } else {
-//       return done(null, user);
-//     }
-//   })
-//   .catch((err) => { return done(err); });
-// }));
+router.post("/signup", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const {
+      rows,
+    } = await db.query(
+      "INSERT INTO users (username, email, password) VALUES ($1, $2, crypt($3, gen_salt('bf', 10))) RETURNING *",
+      [username, email, password]
+    );
+    res.status(200).json(rows[0]);
+  } catch (err) {
+    res.status(400).json({ msg: err.message });
+  }
+});
 
 router.post("/login", function (req, res, next) {
   passport.authenticate("local", function (err, user, info) {
