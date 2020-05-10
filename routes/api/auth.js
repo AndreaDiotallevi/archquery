41b6544fbf380556bc1d3531a8d6fc681f4766b3
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-10 11:35:00 +0100

    Change signup and login strategy

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,12 +7,12 @@ module.exports = router;
 router.post("/signup", async (req, res) => {
   try {
     console.log(req.body);
-    const { displayName, email, password } = req.body;
+    const { username, email, password } = req.body;
     const {
       rows,
     } = await db.query(
-      "INSERT INTO users (display_name, email, password_hash) VALUES ($1, $2, crypt($3, gen_salt('bf', 10))) RETURNING *",
-      [displayName, email, password]
+      "INSERT INTO users (username, email, password) VALUES ($1, $2, crypt($3, gen_salt('bf', 10))) RETURNING *",
+      [username, email, password]
     );
     res.status(200).json(rows[0]);
   } catch (err) {
@@ -22,12 +22,12 @@ router.post("/signup", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { username, password } = req.body;
     const {
       rows,
     } = await db.query(
-      "SELECT * FROM users WHERE email = $1 AND password_hash = crypt($2, password_hash)",
-      [email, password]
+      "SELECT * FROM users WHERE username = $1 AND password = crypt($2, password)",
+      [username, password]
     );
     res.status(200).json(rows[0]);
   } catch (err) {
