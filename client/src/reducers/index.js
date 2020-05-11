50ba4dbc0d1b0fc376de63274857784ab6e284f8
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-11 23:13:37 +0100

    Add authReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import questionsReducers from "./questionsReducers";
 import usersReducers from "./usersReducers";
+import authReducers from "./authReducers";
 
 export default combineReducers({
   questions: questionsReducers,
   users: usersReducers,
   form: formReducer,
+  auth: authReducers,
 });
