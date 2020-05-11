6a1993683c9c2e2ba325e040835ebfd9ae08f9e9
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-12 00:15:58 +0100

    Refactor usersReducers

--- client/src/reducers/usersReducers.js
+++ client/src/reducers/usersReducers.js
@@ -1,10 +1,8 @@
-import { USER_FETCHED, SIGN_UP } from "../actions/types";
+import { USER_FETCHED } from "../actions/types";
 
 export default (state = {}, action) => {
   switch (action.type) {
     case USER_FETCHED:
-      return { ...state, [action.payload.id]: action.payload };
-    case SIGN_UP:
       return { ...state, [action.payload.id]: action.payload };
     default:
       return state;
