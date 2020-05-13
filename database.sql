b950c77b63e133c97ddefc5f6e7bb079807e7501
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-13 15:48:14 +0100

    Add parent_id column to posts table

--- database.sql
+++ database.sql
@@ -4,10 +4,12 @@ CREATE EXTENSION pgcrypto;
 
 CREATE TABLE posts(
   id SERIAL PRIMARY KEY,
-  title VARCHAR(150) NOT NULL,
+  title VARCHAR(150),
   body TEXT NOT NULL,
   creation_date TIMESTAMP NOT NULL DEFAULT NOW(),
-  owner_user_id INTEGER REFERENCES users(id) 
+  owner_user_id INTEGER REFERENCES users(id),
+  post_type_id INTEGER REFERENCES post_types(id)
+  parent_id INTEGER REFERENCES posts(id)
 );
 
 CREATE TABLE users(
@@ -16,4 +18,9 @@ CREATE TABLE users(
   email TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
   creation_date TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE post_types(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
 );
