ffccc0e694757bd05452bc6a4f56894f3e864dc9
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-13 15:49:07 +0100

    Add answersReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import questionsReducer from "./questionsReducer";
+import answersReducers from "./answersReducers";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 
 export default combineReducers({
   questions: questionsReducer,
+  answers: answersReducers,
   users: usersReducer,
   form: formReducer,
   auth: authReducer,
