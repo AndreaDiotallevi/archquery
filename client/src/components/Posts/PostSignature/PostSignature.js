7b6bbce9505cfeb88fd6e4dd6a516f7431c4ef67
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-17 09:35:52 +0100

    Pass postTypeId to relativeTime component

--- client/src/components/Posts/PostSignature/PostSignature.js
+++ client/src/components/Posts/PostSignature/PostSignature.js
@@ -5,7 +5,10 @@ import PostOwnerName from "../PostOwnerName/PostOwnerName";
 const PostSignature = ({ post }) => {
   return (
     <div className="component-post-signature">
-      <PostRelativeTime creationDate={post.creation_date} />
+      <PostRelativeTime
+        creationDate={post.creation_date}
+        postTypeId={post.post_type_id}
+      />
       <PostOwnerName ownerUserId={post.owner_user_id} />
     </div>
   );
