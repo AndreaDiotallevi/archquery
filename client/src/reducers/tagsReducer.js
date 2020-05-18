14b23ee8f9259dd730ba7fb52139f536fe3c7a02
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-18 19:11:21 +0100

    Add TAG_FETCHED action

--- client/src/reducers/tagsReducer.js
+++ client/src/reducers/tagsReducer.js
@@ -1,7 +1,9 @@
-import {} from "../actions/types";
+import { TAG_FETCHED } from "../actions/types";
 
 export default (state = [], action) => {
   switch (action.type) {
+    case TAG_FETCHED:
+      return [...state, action.payload];
     default:
       return state;
   }
