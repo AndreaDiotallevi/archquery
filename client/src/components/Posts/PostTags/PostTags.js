fda6f61764f80a436d80ae4963fa6a6e8c73bd38
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-18 19:15:03 +0100

    Add anchor tag for filtering by tag

--- client/src/components/Posts/PostTags/PostTags.js
+++ client/src/components/Posts/PostTags/PostTags.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PostTags = ({ post }) => {
   if (!post.tags) {
@@ -6,9 +7,11 @@ const PostTags = ({ post }) => {
   }
 
   return (
-    <div>
+    <div className="component-post-tags">
       {post.tags.map((tag) => (
-        <p key={tag}>{tag}</p>
+        <Link to={`/questions/tagged/${tag}`} key={tag}>
+          {tag}
+        </Link>
       ))}
     </div>
   );
