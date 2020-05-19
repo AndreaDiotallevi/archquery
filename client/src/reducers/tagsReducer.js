17d5feb4ed9cec2154bcfc80575dbe460c74dddb
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-19 17:04:57 +0100

    Add tagsReducer

--- client/src/reducers/tagsReducer.js
+++ client/src/reducers/tagsReducer.js
@@ -1,9 +1,21 @@
-import { TAG_FETCHED } from "../actions/types";
+import _ from "lodash";
+import {
+  TAGS_FETCHED,
+  TAG_FETCHED,
+  TAG_CREATED,
+  TAGS_CREATED,
+} from "../actions/types";
 
-export default (state = [], action) => {
+export default (state = {}, action) => {
   switch (action.type) {
+    case TAGS_FETCHED:
+      return { ...state, ..._.mapKeys(action.payload, "name") };
     case TAG_FETCHED:
-      return [...state, action.payload];
+      return { ...state, [action.payload.name]: action.payload };
+    case TAG_CREATED:
+      return { ...state, [action.payload.name]: action.payload };
+    case TAGS_CREATED:
+      return { ...state, ..._.mapKeys(action.payload, "name") };
     default:
       return state;
   }
