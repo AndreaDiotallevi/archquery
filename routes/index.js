df21b3db01bff4900e2414d600ba2cd8fb164ecc
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-19 17:10:25 +0100

    Add postsTags and tagCollection to root route file

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const posts = require("./api/posts");
 const users = require("./api/users");
 const tags = require("./api/tags");
+const tagCollection = require("./api/tagCollection");
+const postsTags = require("./api/postsTags");
 const auth = require("./api/auth");
 
 module.exports = (app) => {
   app.use("/api/posts", posts);
   app.use("/api/users", users);
   app.use("/api/tags", tags);
+  app.use("/api/tagCollection", tagCollection);
+  app.use("/api/postsTags", postsTags);
   app.use("/api/auth", auth);
 };
