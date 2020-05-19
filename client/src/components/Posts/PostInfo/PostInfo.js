80f1f96b5f60114d72693f0b358ed4fc2600057e
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-19 23:00:20 +0100

    Amend styling

--- client/src/components/Posts/PostInfo/PostInfo.js
+++ client/src/components/Posts/PostInfo/PostInfo.js
@@ -1,12 +1,16 @@
 import React from "react";
 import PostMenu from "../PostMenu/PostMenu";
+import PostTags from "../PostTags/PostTags";
 import PostSignature from "../PostSignature/PostSignature";
 
 const PostInfo = ({ post }) => {
   return (
     <div className="component-post-info">
       <PostMenu post={post} />
-      <PostSignature post={post} />
+      <div>
+        <PostTags post={post} />
+        <PostSignature post={post} />
+      </div>
     </div>
   );
 };
