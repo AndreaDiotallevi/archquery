615e6e15b742ebb12315e9b1a21612a0cc3a2714
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-21 16:23:52 +0100

    Add number of answers

--- client/src/components/Posts/AnswerList/AnswerList.js
+++ client/src/components/Posts/AnswerList/AnswerList.js
@@ -4,13 +4,26 @@ import { fetchPostsAndUsers } from "../../../actions";
 import PostLayout from "../PostLayout/PostLayout";
 
 class AnswerList extends React.Component {
-  componentDidMount() {
+  componentDidMount = () => {
     this.props.fetchPostsAndUsers(2, this.props.questionId);
-  }
+  };
+
+  renderHeader = () => {
+    const numberOfAnswers = this.props.answers.length;
+
+    if (numberOfAnswers > 0) {
+      return (
+        <h1>
+          {numberOfAnswers} Answer{numberOfAnswers > 1 ? "s" : ""}
+        </h1>
+      );
+    }
+  };
 
   render() {
     return (
       <div className="component-answer-list">
+        {this.renderHeader()}
         <div className="container-answer-list">
           {this.props.answers.map((post) => (
             <PostLayout post={post} key={post.id} />
