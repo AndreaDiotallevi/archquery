a3606b9b8abe29a4054d5bead80c262389dad97a
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-21 21:14:26 +0100

    Amend PostAnswers component

--- client/src/components/Posts/PostAnswers/PostAnswers.js
+++ client/src/components/Posts/PostAnswers/PostAnswers.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-const PostStats = ({ post }) => {
+const PostAnswers = ({ post }) => {
   const numberOfAnswers = post.answer_count;
 
   return (
     <div className="component-post-answers">
       <p>{numberOfAnswers}</p>
-      <p>answer{numberOfAnswers > 1 ? "s" : ""}</p>
+      <p>answer{numberOfAnswers !== 1 ? "s" : ""}</p>
     </div>
   );
 };
 
-export default PostStats;
+export default PostAnswers;
