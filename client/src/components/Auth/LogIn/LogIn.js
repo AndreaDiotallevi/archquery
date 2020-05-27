f580e33bcadd67bf5031be65332ce2c451d45499
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-27 16:19:29 +0100

    Refactor login component

--- client/src/components/Auth/LogIn/LogIn.js
+++ client/src/components/Auth/LogIn/LogIn.js
@@ -5,15 +5,6 @@ import { logIn } from "../../../actions";
 import LogInForm from "../LogInForm/LogInForm";
 
 class LogIn extends React.Component {
-  renderInput = ({ input, label, meta }) => {
-    return (
-      <div>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-      </div>
-    );
-  };
-
   onSubmit = (formValues) => {
     this.props.logIn(formValues);
   };
