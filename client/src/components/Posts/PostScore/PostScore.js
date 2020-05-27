9e7f97ec49e7ca72b7afe219d1b5ddd38b24d58f
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-27 19:15:55 +0100

    Add tests for PostScore component

--- client/src/components/Posts/PostScore/PostScore.js
+++ client/src/components/Posts/PostScore/PostScore.js
@@ -4,7 +4,7 @@ const PostScore = ({ post }) => {
   const score = post.score;
 
   return (
-    <div className="component-post-score">
+    <div className="component-post-score" data-test="component-post-score">
       <p>{score}</p>
       <p>vote{score !== 1 ? "s" : ""}</p>
     </div>
---
+++ client/src/components/Posts/PostScore/PostScore.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import { findByTestAttr } from "../../../../test/testUtils";
+import PostScore from "./PostScore";
+
+const setup = (props = {}) => {
+  return shallow(<PostScore {...props} />);
+};
+
+describe("PostScore", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup({ post: { score: 1 } });
+  });
+
+  test("renders without errors", () => {
+    expect(wrapper.length).toEqual(1);
+  });
+
+  test("renders the main div", () => {
+    const component = findByTestAttr(wrapper, "component-post-score");
+    expect(component.length).toBe(1);
+  });
+
+  test("renders the 'score' text if score is 1", () => {
+    wrapper = setup({ post: { score: 1 } });
+    const component = findByTestAttr(wrapper, "component-post-score");
+    expect(component.children().last().text()).toEqual("vote");
+  });
+
+  test("renders the 'scores' text if score is more than 1", () => {
+    wrapper = setup({ post: { score: 2 } });
+    const component = findByTestAttr(wrapper, "component-post-score");
+    expect(component.children().last().text()).toEqual("votes");
+  });
+});
