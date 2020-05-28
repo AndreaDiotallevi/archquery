3bdb5165f97025f8c09c47924bd3db11f3575875
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-28 20:36:07 +0100

    Refactor test

--- client/src/components/Posts/PostSummary/PostSummary.test.js
+++ client/src/components/Posts/PostSummary/PostSummary.test.js
@@ -4,18 +4,9 @@ import { shallow } from "enzyme";
 import { findByTestAttr } from "../../../../test/testUtils";
 import PostSummary from "./PostSummary";
 
-const setup = (props = {}) => {
-  return shallow(<PostSummary {...props} />);
-};
-
 describe("PostSummary", () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = setup({ post: {} });
-  });
-
   test("renders the main div", () => {
+    const wrapper = shallow(<PostSummary />);
     const component = findByTestAttr(wrapper, "component-post-summary");
     expect(component.length).toBe(1);
   });
