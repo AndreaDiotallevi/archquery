89e466b79ca32117c43bba216137a1e52005ba47
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-31 17:35:45 +0100

    Refactor PostExcerpt component

--- client/src/components/Posts/PostExcerpt/PostExcerpt.js
+++ client/src/components/Posts/PostExcerpt/PostExcerpt.js
@@ -2,6 +2,10 @@ import React from "react";
 
 const PostExcerpt = ({ post }) => {
   const { body } = post;
+  // const div = document.createElement("div");
+  // div.innerHTML = body;
+  // const text = div.textContent || div.innerText || "";
+  const text = body.replace(/<\/?[^>]+(>|$)/g, " ");
 
   return (
     <div
@@ -10,7 +14,7 @@ const PostExcerpt = ({ post }) => {
     >
       <div
         dangerouslySetInnerHTML={{
-          __html: `${body.substring(0, 200)}${body.length > 200 ? "..." : ""}`,
+          __html: `${text.substring(0, 200)}${text.length > 200 ? "..." : ""}`,
         }}
       />
     </div>
