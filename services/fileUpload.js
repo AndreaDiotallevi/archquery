1079dc7ed2360d8edcfd4432b7c10716e99f61e5
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-06-07 17:52:43 +0100

    Amend aws s3 bucket name

--- services/fileUpload.js
+++ services/fileUpload.js
@@ -13,10 +13,7 @@ const s3 = new aws.S3();
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket:
-      process.env.NODE_ENV === "production"
-        ? "archquery-production"
-        : "archquery",
+    bucket: "archquery",
     acl: "public-read",
     metadata: function (req, file, cb) {
       cb(null, { fieldName: file.fieldname });
