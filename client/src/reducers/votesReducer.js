9514620de58f02e85e04f263223ec90c2d933ed7
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-06-11 15:52:40 +0100

    Add delete vote to reducer

--- client/src/reducers/votesReducer.js
+++ client/src/reducers/votesReducer.js
@@ -1,4 +1,4 @@
-import { VOTES_FETCHED, VOTE_CREATED } from "../actions/types";
+import { VOTES_FETCHED, VOTE_CREATED, VOTE_DELETED } from "../actions/types";
 
 export default (state = [], action) => {
   switch (action.type) {
@@ -6,6 +6,8 @@ export default (state = [], action) => {
       return action.payload;
     case VOTE_CREATED:
       return [...state, action.payload];
+    case VOTE_DELETED:
+      return state.filter((vote) => vote.id !== action.payload);
     default:
       return state;
   }
