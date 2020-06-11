b8e0a78ee36edfe8e7d948be9c8dd442a02c7c4e
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-06-11 15:53:00 +0100

    Amend voting button to be a toggle feature

--- client/src/components/Posts/PostVote/PostVote.js
+++ client/src/components/Posts/PostVote/PostVote.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
-import { upvotePost, downvotePost } from "../../../actions";
+import { votePost } from "../../../actions";
 
 const PostVote = (props) => {
   const { post } = props;
 
   return (
     <div className="component-post-vote" data-test="component-post-vote">
-      <button onClick={() => props.upvotePost(post)}>
+      <button onClick={() => props.votePost(post, props.userId)}>
         <svg>
           <path d="M2 26h32L18 10 2 26z"></path>
         </svg>
       </button>
       <p>{post.score}</p>
-      <button onClick={() => props.downvotePost(post)}>
-        <svg>
-          <path d="M2 10h32L18 26 2 10z"></path>
-        </svg>
-      </button>
     </div>
   );
 };
 
-export default connect(null, { upvotePost, downvotePost })(PostVote);
+const mapStateToProps = (state) => {
+  return {
+    userId: state.auth.userId,
+  };
+};
+
+export default connect(mapStateToProps, { votePost })(PostVote);
