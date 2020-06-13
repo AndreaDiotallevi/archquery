1dcc0b5566893c3a399ac2a2613b6dee27683d8c
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-06-13 09:28:19 +0100

    Add thumbsup image'

--- client/src/components/Posts/PostVote/PostVote.js
+++ client/src/components/Posts/PostVote/PostVote.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { votePost, fetchVote } from "../../../actions";
+import ThumbsUp from "../../../assets/thumbs-up.png";
 
 class PostVote extends React.Component {
   componentDidMount = () => {
@@ -27,7 +28,11 @@ class PostVote extends React.Component {
     return (
       <div className="component-post-vote" data-test="component-post-vote">
         <button onClick={this.handleClick}>
-          <i className={`fa fa-thumbs-up ${isVoted ? "voted" : ""}`}></i>
+          <img
+            src={ThumbsUp}
+            alt="thumbs-up"
+            className={`thumbs-up ${isVoted ? "voted" : ""}`}
+          ></img>
         </button>
         <p>{post.score}</p>
       </div>
