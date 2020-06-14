e9c8eb8c1311a96d9688464a552be25220c89767
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-06-14 11:43:13 +0100

    Delete commented out code

--- client/src/components/Posts/PostExcerpt/PostExcerpt.js
+++ client/src/components/Posts/PostExcerpt/PostExcerpt.js
@@ -2,9 +2,6 @@ import React from "react";
 
 const PostExcerpt = ({ post }) => {
   const { body } = post;
-  // const div = document.createElement("div");
-  // div.innerHTML = body;
-  // const text = div.textContent || div.innerText || "";
   const text = body.replace(/<\/?[^>]+(>|$)/g, " ");
 
   return (
